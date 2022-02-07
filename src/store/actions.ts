a058ea0339df4b9c9d5340bfc7d5a05eb35e6de7
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2022-02-07 21:34:00 +0100

    commented extra action

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -4,7 +4,7 @@ const makeAction = createActionMaker<typeof Actions>()
 
 const Actions = {
   SET_NICKNAME: (payload: string) => makeAction("SET_NICKNAME", payload),
-  SET_SOCKET: (payload: number) => makeAction("SET_SOCKET", payload),
+//   SET_SOCKET: (payload: number) => makeAction("SET_SOCKET", payload),
 };
 
 export type Action = SingleAction<typeof Actions>
