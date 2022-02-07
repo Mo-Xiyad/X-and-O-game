54beca397b2e269235fc851b4536af93596633b2
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2022-02-07 21:00:59 +0100

    Enter component with form

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import './App.scss';
-import { Routes,Route } from 'react-router-dom';
-import { routes } from './routes';
+import React from "react";
+import "./App.scss";
+import { Routes, Route } from "react-router-dom";
+import { routes } from "./routes";
 
 function App() {
   return (
-    <Routes>
-      {routes.map(([path, Element]) =>
-        <Route path={path} element={<Element />}/>
-      )}
-    </Routes>
+    <div id="enter" className="h-screen w-screen bg-primary">
+      <Routes>
+        {routes.map(([path, Element]) => (
+          <Route path={path} element={<Element />} />
+        ))}
+      </Routes>
+    </div>
   );
 }
 
--- src/components/Enter.tsx
+++ src/components/Enter.tsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import { useNavigate } from "react-router";
 
 const Enter = () => {
+  const [name, setName] = useState("");
+  const navigate = useNavigate()
+
+  const handleSubmit= ()=>{
+    navigate("/play");
+  }
+
+  useEffect(() =>{
+    console.log(name);
+    
+  })
   return (
     <div
       id="enter"
@@ -14,11 +26,10 @@ const Enter = () => {
           </h1>
         </div>
         <div className="w-full max-w-xs">
-          <form className="bg-gray-400 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <form className="bg-gray-400 shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
             <div className="mb-4 ">
               <label
                 className="block text-gray-700 text-md font-bold flex items-center justify-center mb-5 antialiased font-mono"
-                htmlFor="username"
               >
                 Player Name
               </label>
@@ -26,6 +37,8 @@ const Enter = () => {
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline antialiased font-mono"
                 id="username"
                 type="text"
+                value={name}
+                onChange={e => setName(e.target.value)}
                 placeholder="Name"
               />
             </div>
@@ -33,7 +46,7 @@ const Enter = () => {
             <div className="flex items-center justify-center">
               <button
                 className="bg-secondary hover:bg-rose-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline antialiased font-mono"
-                type="button"
+                type="submit"
               >
                 Play
               </button>
