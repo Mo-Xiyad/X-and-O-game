4284abd43d1f62daf5be58d6ff28e88fad423095
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2022-02-08 22:24:21 +0100

    App modal

---
+++ src/atoms/modalState.ts
@@ -0,0 +1,10 @@
+
+import { atom } from "recoil";
+
+export const modalState = atom<AppModal>({
+  key: "modalState",
+  default: {
+    message: "",
+    display: false,
+  },
+});
---
+++ src/components/AppModal/Appmodal.d.ts
@@ -0,0 +1,4 @@
+type AppModal = {
+  message: string;
+  display: boolean;
+};
