d3e8b72564e60cc63f856a2a184289107f434c6f
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2022-02-10 23:22:49 +0100

    sockets connected

--- src/components/Game/Board.tsx
+++ src/components/Game/Board.tsx
@@ -15,129 +15,109 @@ interface Opponent {
 
 
 export default function Board() {
-     const [matrix, setMatrix] = useState<[Symbol, Symbol, Symbol][]>([
-       //00    01    02
-       [null, null, null],
-       //10    11    12
-       [null, null, null],
-       //20    21    22
-       [null, null, null],
-     ]);
+      const [matrix, setMatrix] = useState<[Symbol, Symbol, Symbol][]>([
+        //00    01    02
+        [null, null, null],
+        //10    11    12
+        [null, null, null],
+        //20    21    22
+        [null, null, null],
+      ]);
 
-     const name = useRecoilValue(nameState);
+      const name = useRecoilValue(nameState);
 
-     const setModal = useSetRecoilState(modalState);
+      const setModal = useSetRecoilState(modalState);
 
-     const [opponent, setOpponent] = useState<Opponent | null>(null);
+      const [opponent, setOpponent] = useState<Opponent | null>(null);
 
-     const [symbol, setSymbol] = useRecoilState(symbolState);
+      const [symbol, setSymbol] = useRecoilState(symbolState);
 
-     const handleMatrixUpdate = (x: number, y: number) => {
-       if (!!matrix[y][x]) return;
+      const handleMatrixUpdate = (x: number, y: number) => {
+        if (!!matrix[y][x]) return;
 
-       const newMatrix = [...matrix];
-       newMatrix[y][x] = symbol;
-       setMatrix(newMatrix);
+        const newMatrix = [...matrix];
+        newMatrix[y][x] = symbol;
+        setMatrix(newMatrix);
 
-       // console.log({ opponent })
-       if (!opponent) {
-         // should never happen
-         throw new Error("OPPONENT CAN'T BE NULL");
-       }
+        // console.log({ opponent })
+        if (!opponent) {
+          // should never happen
+          throw new Error("OPPONENT CAN'T BE NULL");
+        }
 
-       socketClient.emit("matrixUpdate", { matrix: newMatrix, opponent });
-     };
+        socketClient.emit("matrixUpdate", { matrix: newMatrix, opponent });
+      };
 
-     useEffect(() => {
-       socketClient.on("connect", () => {
-         console.log("connected...");
-         console.log(name);
-         socketClient.emit("loggedIn", { name, symbol });
-       });
+      useEffect(() => {
+        socketClient.on("connect", () => {
+          console.log("connected...");
+          console.log(name);
+          socketClient.emit("loggedIn", { name, symbol });
+        });
 
-       socketClient.on("waitingForOpponent", () => {
-         setModal({
-           message: "Waiting for another player...",
-           display: true,
-         });
-       });
-
-       socketClient.on("gameStarted", ({ opponent, symbol }) => {
-         setSymbol(symbol);
-         setOpponent(opponent);
-
-         if (symbol === "X") {
-           setModal({
-             message: "Waiting for your opponent move...",
-             display: true,
-           });
-         } else {
-           setModal((t) => ({ ...t, display: false }));
-         }
-       });
-
-       socketClient.on("waitingForMove", () => {
-         setModal({
-           message: "Waiting for your opponent...",
-           display: true,
-         });
-       });
-
-       socketClient.on("yourTurn", ({ matrix }) => {
-         setModal((t) => ({
-           ...t,
-           display: false,
-         }));
-
-         setMatrix(matrix);
-       });
-     }, [setModal]);
-
-useEffect(() => {
-          socketClient.on("connect", () => {
-            console.log("connected...");
-            console.log(name);
-            socketClient.emit("loggedIn", { name, symbol });
+        socketClient.on("waitingForOpponent", () => {
+          setModal({
+            message: "Waiting for another player...",
+            display: true,
           });
-           socketClient.on("waitingForOpponent", () => {
-             setModal({
-               message: "Waiting for another player...",
-               display: true,
-             });
-           });
-})
+        });
 
-     useEffect(() => {
-       const gameOver = ({
-         winner,
-         matrix,
-       }: {
-         winner: Symbol;
-         matrix: [Symbol, Symbol, Symbol][];
-       }) => {
-         const draw = winner === null;
-         const won = winner === symbol;
+        socketClient.on("gameStarted", ({ opponent, symbol }) => {
+          setSymbol(symbol);
+          setOpponent(opponent);
 
-         setMatrix(matrix);
+          if (symbol === "X") {
+            setModal({
+              message: "Waiting for your opponent move...",
+              display: true,
+            });
+          } else {
+            setModal((t) => ({ ...t, display: false }));
+          }
+        });
 
-         setModal({
-           message: draw ? "Draw!" : won ? "You won!" : "You lost!",
-           display: true,
-         });
-       };
-       socketClient.on("gameOver", gameOver);
+        socketClient.on("waitingForMove", () => {
+          setModal({
+            message: "Waiting for your opponent...",
+            display: true,
+          });
+        });
 
-       return () => {
-         socketClient.off("gameOver", gameOver);
-       };
-     }, [symbol]);
+        socketClient.on("yourTurn", ({ matrix }) => {
+          setModal((t) => ({
+            ...t,
+            display: false,
+          }));
 
+          setMatrix(matrix);
+        });
+      }, [setModal]);
 
-    useEffect(() => {
-      AOS.init({
-        duration: 2000,
-      });
-    }, []);
+      useEffect(() => {
+        const gameOver = ({
+          winner,
+          matrix,
+        }: {
+          winner: Symbol;
+          matrix: [Symbol, Symbol, Symbol][];
+        }) => {
+          const draw = winner === null;
+          const won = winner === symbol;
+
+          setMatrix(matrix);
+
+          setModal({
+            message: draw ? "Draw!" : won ? "You won!" : "You lost!",
+            display: true,
+          });
+        };
+        socketClient.on("gameOver", gameOver);
+
+        return () => {
+          socketClient.off("gameOver", gameOver);
+        };
+      }, [symbol]);
+
 
   return (
     <>
